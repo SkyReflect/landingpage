64f04c94a83ff7410ba066ac611b0645db52a12a
Perwollnt <Proedge1@skyreflect.hu>
2022-11-07 21:10:18 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+
+
+import { NgParticlesModule } from "ng-particles";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgParticlesModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
